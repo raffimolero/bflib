075be1f57cb40358845d4bb2caa87cc64072d4ab
Raffi Molero <raffimolero@gmail.com>
2025-04-07 02:45:05 +0800

    switchy

--- main.py
+++ main.py
@@ -6,26 +6,44 @@ from bf import *
 #     bf_print('World!', True, 0),
 # )
 res = f"""
-    ,[ (
+    {move(8)}
+    -
+    >+
+    [
+        >>
+        >,
         {switch(
-            3,
-            {
-                '!': puts('bang'),
-                '<': puts('left'),
-                '>': puts('right'),
-                '+': puts('plus'),
-                '-': puts('minus'),
-                ',': puts('comma'),
-                '.': puts('dot'),
-                '[': puts('open'),
-                ']': puts('close'),
-            },
-            reset() + puts('default')
+            1,
+            {c: f'<<{add(i + 1)}>>' for i, c in enumerate('><+-[].,')} | {'!': '', 0: ''},
+            reset() + '<<'
         )}
-    ) ,]
+        <
+    ]
+    >
+    +[<<+] seek to instruction pointer
+    >[
+        {clone_to(-1, -3)}
+        {move(-1)}
+        {clone_to(1)}
+        {move(-2)}
+        {switch(
+            2,
+            {
+                1: puts('right'),
+                2: puts('left'),
+                3: puts('add'),
+                4: puts('sub'),
+                5: puts('open'),
+                6: puts('close'),
+                7: puts('out'),
+                8: puts('in'),
+            }
+        )}
+        {move(5)}
+    ]
+
 """
 print(bf_format(res))
-
 
 
 # res = add(20) + clone_to(1, 3, (2, 5)) * MotionFactor(-2)
